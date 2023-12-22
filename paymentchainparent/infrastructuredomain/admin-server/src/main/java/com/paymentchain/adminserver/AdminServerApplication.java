66802638d20199da0ba2bff433f9f6097521345a
Paul Flores <paulfloresdev@icloud.com>
2023-12-22 17:44:54 -0600

    update filterChain configuration for spring-boot 3.1.7

--- paymentchainparent/infrastructuredomain/admin-server/src/main/java/com/paymentchain/adminserver/AdminServerApplication.java
+++ paymentchainparent/infrastructuredomain/admin-server/src/main/java/com/paymentchain/adminserver/AdminServerApplication.java
@@ -2,7 +2,6 @@ package com.paymentchain.adminserver;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
