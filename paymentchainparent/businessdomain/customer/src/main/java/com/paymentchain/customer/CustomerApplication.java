2d0f0ab627fab45c21acbdd96af31cebeecb5be0
Paul Flores <paulfloresdev@icloud.com>
2023-12-22 17:26:55 -0600

    delete inecesary imports

--- paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/CustomerApplication.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/CustomerApplication.java
@@ -3,13 +3,11 @@ package com.paymentchain.customer;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-
 
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
