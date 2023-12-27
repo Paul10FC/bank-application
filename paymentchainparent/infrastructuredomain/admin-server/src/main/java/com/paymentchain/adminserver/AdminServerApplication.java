f47b7ef9bfec43595c93ef504571cfe465eef47b
Paul Flores <paulfloresdev@icloud.com>
2023-12-27 17:12:10 -0600

    remove innecesarty lines

--- paymentchainparent/infrastructuredomain/admin-server/src/main/java/com/paymentchain/adminserver/AdminServerApplication.java
+++ paymentchainparent/infrastructuredomain/admin-server/src/main/java/com/paymentchain/adminserver/AdminServerApplication.java
@@ -3,7 +3,6 @@ package com.paymentchain.adminserver;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,7 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @EnableAdminServer
-@EnableEurekaClient
 public class AdminServerApplication {
 
 	public static void main(String[] args) {
